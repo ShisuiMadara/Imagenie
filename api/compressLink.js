b0e0854f403d68ef73a77cf2f6cb62c1c81b2514
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2023-01-07 01:47:28 +0530

    added compression and metadata link based

---
+++ api/compressLink.js
@@ -0,0 +1,42 @@
+var axios = require('axios');
+require('dotenv').config()
+
+//reads link output file
+
+async function compress(req,res) {
+    
+    req = req.body
+
+    console.log(req)
+
+    var config = {
+        method: 'post',
+        url: 'https://api.apyhub.com/processor/image/compress/url/file?output=test-sample.png&quality=90',
+        headers: { 
+            'apy-token': process.env.compressToken, 
+            'Content-Type': 'application/json'
+        },
+        data : req
+    };
+
+    await axios(config)
+    .then(async function (response) {
+        console.log(JSON.stringify(response.data));
+        await res.send({
+            sucess:true,
+            data: response.data
+        })
+    })
+    .catch(function (error) {
+        console.log(error);
+
+        res.send({
+            success: false,
+            data: error
+        })
+    });
+
+}
+
+exports.execute = compress
+
