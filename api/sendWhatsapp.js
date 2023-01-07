1f051d981410891e7c8537d01f49213402d5ddef
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2023-01-07 18:22:05 +0530

    added whatsapp message utility

---
+++ api/sendWhatsapp.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const account_sid = process.env.twilioAcc
+const account_token = process.env.twilioToken
+
+console.log(account_sid)
+console.log(process.env.cropToken)
+
+const client = require('twilio')(account_sid, account_token);
+
+async function sms (req,res) {
+    client.messages 
+      .create({ 
+         body: 'Hello, please find your final image at ' + req.url, 
+         from: 'whatsapp:+14155238886',       
+         to: 'whatsapp:'+req.phone
+       }) 
+       .then(res.send({
+        success:true,
+        data: "Message sent successfully"
+    })).catch(function(error){
+        res.send({
+            success: false,
+            data: error
+        })
+    })
+}
+
+exports.execute = sms
