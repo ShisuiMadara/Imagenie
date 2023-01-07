02c4525fa8768ac27e7daf868152807096b3a091
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2023-01-07 18:09:31 +0530

    added sms utility

---
+++ api/sendsms.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const account_sid = process.env.twilioAcc
+const account_token = process.env.twilioToken
+
+console.log(account_sid)
+console.log(process.env.cropToken)
+
+const client = require('twilio')(account_sid, account_token);
+
+async function sms (req,res) {
+    client.messages.create({
+        to: req.phoneNum,
+        from: '+1 205 839 4449',
+        body: 'Hello, please find you image at ' + req.url
+    }).then(res.send({
+        success:true,
+        data: "Message sent successfully"
+    })).catch(function(error){
+        res.send({
+            success: false,
+            data: error
+        })
+    })
+}
+
+exports.execute = sms
